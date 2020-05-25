97930cf4c2f84e5f9f9c06f07a2bd4031c6fd641
Michael Jones <djyoungjones@gmail.com>
2020-05-25 22:18:48 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
 def greet_characters(array)
  index = 0
-  array.each_with_index {|name, index| puts "#{index + 1}.  Hello  #{name}! "}
+  array.each_with_index {|name, index| puts "#{index + 1}.  Hello #{name}! "}
 end
 
 
